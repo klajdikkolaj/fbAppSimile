6271b351457d7929f4f1642edbf514ca940e402d
klajdikolaj <klajdi.kolaj@siemplify.co>
2020-05-06 16:44:38 +0200

    first state transformation and axos api call

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    value :[]
+  }
+  componentDidMount(): void {
+    axios.get('localhost:5000/api/values')
+        .then((response)=>{
+          this.setState({
+            value:response.data
+          })
+        })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <ul>
+              {this.state.value.map((value:any) => (
+                <li>{value.name}</li>
+              ))}
+            </ul>
+
+          </header>
+        </div>
+    );
+    
+  }
+
+ 
 }
 
 export default App;
