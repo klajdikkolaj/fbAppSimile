89551474538d3c5bed396986c74288cfe397849f
klajdikolaj <klajdi.kolaj@siemplify.co>
2020-05-06 11:12:51 +0200

    first commits in clone

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Values> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Values>().HasData(
+                new Values {Id = 1, Name = "First"},
+                new Values {Id = 2, Name = "Second"},
+                new Values {Id = 3, Name = "Third"}
+                );
+        }
+    }
+}
